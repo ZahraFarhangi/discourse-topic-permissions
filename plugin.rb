f985775538293231210145b94148d577e0a2110f
ZahraFarhangi <126955572+ZahraFarhangi@users.noreply.github.com>
2024-12-19 16:23:21 +0330

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -1,46 +1,32 @@
-# plugin.rb
-# frozen_string_literal: true
+# name: discourse-topic-permissions
+# about: A plugin to add topic-specific access permissions
+# version: 0.1
+# authors: Zahra Farhangi
+# url: https://github.com/ZahraFarhangi/discourse-topic-permissions
 
 enabled_site_setting :topic_permissions_enabled
 
-register_asset "javascripts/discourse/initializers/topic-permissions.js"
-
 after_initialize do
+  # Register custom field for topic
   Topic.register_custom_field_type('allowed_user_ids', :array)
-  Topic.register_custom_field_type('allowed_group_ids', :array)
 
+  # Serializer to expose custom field
   add_to_serializer(:topic_view, :allowed_user_ids) do
     object.topic.custom_fields['allowed_user_ids']
   end
 
-  add_to_serializer(:topic_view, :allowed_group_ids) do
-    object.topic.custom_fields['allowed_group_ids']
+  # Event to save allowed users
+  DiscourseEvent.on(:topic_created) do |topic, params, user|
+    allowed_users = params[:allowed_user_ids] || []
+    topic.custom_fields['allowed_user_ids'] = allowed_users
+    topic.save_custom_fields
   end
 
-  Discourse::Application.routes.append do
-    post "/topic-permissions" => "topic_permissions#save"
-  end
-
-  class ::TopicPermissionsController < ApplicationController
-    def save
-      topic = Topic.find_by(id: params[:topic_id])
-      raise Discourse::NotFound if topic.blank?
-
-      topic.custom_fields['allowed_user_ids'] = params[:users]
-      topic.custom_fields['allowed_group_ids'] = params[:groups]
-      topic.save_custom_fields
-      render json: success_json
-    end
-  end
-
+  # Custom Guardian logic
   TopicGuardian.class_eval do
     def can_see_topic?(topic)
-      allowed_users = topic.custom_fields['allowed_user_ids'] || []
-      allowed_groups = topic.custom_fields['allowed_group_ids'] || []
-
-      return true if allowed_users.include?(user.id.to_s)
-      return true if allowed_groups.any? { |g| user.groups.pluck(:id).include?(g.to_i) }
-
+      allowed_users = topic.custom_fields['allowed_user_ids']
+      return true if allowed_users.blank? || allowed_users.include?(user.id)
       false
     end
   end
